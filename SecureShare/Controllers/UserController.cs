463a84fa5802d85244e62925051b3d693b6f6153
stormbreakerbg <georgyangelov@gmail.com>
2012-06-27 23:23:02 +0300

    Simple login panel using Bootstrap's modals

---
+++ SecureShare/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecureShare.Controllers
+{
+    public class UserController : Controller
+    {
+		[HttpPost]
+		public ActionResult Login(Models.Login info)
+		{
+			try
+			{
+				// TODO: Add insert logic here
+
+				return RedirectToAction("Index", "Home");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+
+		public bool EmailAvailable(string email)
+		{
+			return false;
+		} 
+    }
+}
---
+++ SecureShare/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SecureShare.Models
+{
+	public class Login
+	{
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		[Remote("EmailAvailable", "User")]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		public bool RememberMe { get; set; }
+	}
+}
