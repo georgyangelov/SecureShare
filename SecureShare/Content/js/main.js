728f1255ddde3df2b0b43158a8949debbf15daa2
stormbreakerbg <georgyangelov@gmail.com>
2012-06-28 22:43:52 +0300

    Fix for a bug with the tile script and Firefox

--- SecureShare/Content/js/main.js
+++ SecureShare/Content/js/main.js
@@ -6,6 +6,9 @@ function initGrid($)
 			return containerWidth / 6;
 		}
 	});
+
+	// Fix for the script in Firefox 13. It seems to need to recalculate something
+	$('.thumbnails').trigger('resize');
 }
 
 jQuery(initGrid);
