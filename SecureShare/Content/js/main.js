8dc5c790e1d6eb45af5254a29513d091fc0677a3
stormbreakerbg <georgyangelov@gmail.com>
2012-06-29 15:22:59 +0300

    The messages on the grid now expand on click

--- SecureShare/Content/js/main.js
+++ SecureShare/Content/js/main.js
@@ -1,5 +1,8 @@
-function initGrid($)
-{
+function updateGrid() {
+	$('.thumbnails').trigger('resize');
+}
+
+function initGrid($) {
 	$('.thumbnails').masonry({
 		itemSelector: '.span',
 		columnWidth: function (containerWidth) {
@@ -8,7 +11,58 @@ function initGrid($)
 	});
 
 	// Fix for the script in Firefox 13. It seems to need to recalculate something
-	$('.thumbnails').trigger('resize');
+	updateGrid();
 }
 
 jQuery(initGrid);
+
+var testText = 'Message text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about hereMessage text right about here Message text right about here Message text right about here Message text right about here';
+$('.message.active').live('click', function () {
+	var text = '';
+	var $msg = $(this);
+	var $thumbnail = $msg.find('.thumbnail');
+
+	if ($msg.hasClass('span4')) {
+		text = testText;
+
+		$thumbnail
+			.css('width', $thumbnail.width() + 'px')
+			.css('height', $thumbnail.height() + 'px');
+
+		$msg.removeClass('span4').addClass('span8');
+
+		var newHeight = $thumbnail.find('p').parent().textHeight(text, $msg.width()) + $msg.find('h4').height();
+
+		$msg.height(newHeight + 10);
+
+		$thumbnail.animate({ width: $msg.width() - 10, height: newHeight }, 700, function () {
+			updateGrid();
+		});
+
+		$thumbnail.find('p').text(text);
+		updateGrid();
+	}
+	else {
+		var $measure = $('<li><div class="thumbnail"></div></li>').addClass('span4');
+		$msg.parent().append($measure);
+		var span4Width = $measure.find('.thumbnail').width();
+		$measure.remove();
+
+		text = testText.cutToWord(200, '...');
+
+		$thumbnail
+			.css('width', $thumbnail.width() + 'px')
+			.css('height', $thumbnail.height() + 'px');
+
+		var newHeight = $msg.find('p').parent().textHeight(text, span4Width) + $msg.find('h4').height();
+
+		$thumbnail.animate({ width: span4Width, height: newHeight }, 700, function () {
+			$msg.addClass('span4').removeClass('span8');
+			$msg.css('height', $thumbnail.outerHeight() + 'px');
+			$(this).find('p').text(text);
+
+			updateGrid();
+		});
+
+	}
+});
---
+++ SecureShare/Content/js/textHelpers.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+	$.fn.textHeight = function (text, width) {
+		var sensor = $('<p />').addClass('test').css({ display: 'none', width: width, margin: 0, padding: 0 }).text(text);
+		$(this).append(sensor);
+		var height = sensor.height();
+		sensor.remove();
+		return height;
+	};
+})(jQuery);
+
+String.prototype.cutToWord = function (length, dots) {
+	if (typeof dots === 'undefined')
+		dots = '';
+
+	if (this.length <= length)
+		return this;
+
+	for (var i = length; i > 0; i--)
+		if (this.charAt(i).match('[\.,\(\)\-\_\&\!\?\;]') != null)
+			return this.substr(0, i) + dots;
+
+	return this.substr(0, length) + dots;
+}
