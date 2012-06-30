996f5c1c71a5d11bb1fb713225ef4076530f9a90
stormbreakerbg <georgyangelov@gmail.com>
2012-06-30 21:33:56 +0300

    Project upgraded to MVC 4: - Basic REST API placeholder (MVC 4 web api) - Renamed project to ShareGrid

--- SecureShare/Properties/AssemblyInfo.cs
+++ SecureShare/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SecureShare")]
+[assembly: AssemblyTitle("ShareGrid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SecureShare")]
+[assembly: AssemblyProduct("ShareGrid")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c75b421-f9dc-442c-8b6b-3acd10e1eef1")]
+[assembly: Guid("8138e598-2735-4f72-86ff-a2116520309b")]
 
 // Version information for an assembly consists of the following four values:
 //
