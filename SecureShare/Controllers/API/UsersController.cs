6d09b13423309ce6cb22d27d615c45f5afcedfd6
stormbreakerbg <georgyangelov@gmail.com>
2012-07-01 16:42:57 +0300

    Preparing for the API and UI interaction code Renamed 'User' to 'Users' and moved the controller to the API Added amplify.js for the client end of the REST API Added knockout.js and knockout mapping plugin for the UI interaction

---
+++ SecureShare/Controllers/API/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace SecureShare.Controllers
+{
+	public class UsersController : ApiController
+	{
+		// GET api/values/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		public void Post(string value)
+		{
+		}
+
+		// PUT api/values/5
+		public void Put(int id, string value)
+		{
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
