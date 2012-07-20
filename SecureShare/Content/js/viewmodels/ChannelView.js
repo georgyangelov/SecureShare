ec7cd4e6b2ae7b212a339a38b6b10519d0b84278
stormbreakerbg <georgyangelov@gmail.com>
2012-07-20 17:00:40 +0300

    Expanding/contracting messages works

--- SecureShare/Content/js/viewmodels/ChannelView.js
+++ SecureShare/Content/js/viewmodels/ChannelView.js
@@ -14,6 +14,7 @@ function ChannelEntity(data) {
 		data.Link = "";
 
 	/* Properties */
+	this.Id = ko.observable(data.Id);
 	this.Title = ko.observable(data.Title);
 	this.Message = ko.observable(data.Message);
 	this.Link = ko.observable(data.Link);
@@ -22,8 +23,9 @@ function ChannelEntity(data) {
 
 	/* Helper properties */
 	this.smallMessage = ko.computed(function () {
-		return self.Message().cutToWord(200, ' ...');
+		return self.Message().cutToWord(20, ' ...');
 	});
+	this.currentMessage = ko.observable(this.smallMessage());
 
 	this.isExpandable = ko.computed(function () {
 		return self.smallMessage() != self.Message();
@@ -35,18 +37,73 @@ function ChannelEntity(data) {
 			return expanded();
 		},
 		write: function (value) {
-			if (value) {
-				//TODO: Expand the panel
+			if (!self.isExpandable())
+				return;
 
-				expanded(true);
+			var $msg = $("[data-id='" + self.Id() + "']");
+
+			if (value) {
+				var $thumbnail = $msg.find('.thumbnail');
+
+				if ($msg.hasClass('span4')) {
+					$thumbnail
+						.css('width', $thumbnail.width() + 'px')
+						.css('height', $thumbnail.height() + 'px');
+
+					$msg.removeClass('span4').addClass('span8');
+
+					var newHeight = $thumbnail.find('p').parent().textHeight(self.Message(), $msg.width()) + $msg.find('h4').height() + 5;
+
+					$msg.height(newHeight + 10);
+
+					setTimeout(function () {
+						$msg.addClass('expanded');
+						self.currentMessage(self.Message());
+						$thumbnail.find('.icon-resize-full').removeClass('icon-resize-full').addClass('icon-resize-small');
+						$thumbnail.animate({ width: $msg.width() - 10, height: newHeight }, 700, function () {
+							updateGrid();
+
+							expanded(true);
+						});
+					}, 700);
+
+					updateGrid();
+				}
+
 			}
 			else {
-				//TODO: Contract the panel
+				var $thumbnail = $msg.find('.thumbnail');
 
-				expanded(false);
+				var $measure = $('<li><div class="thumbnail"></div></li>').addClass('span4');
+				$msg.parent().append($measure);
+				var span4Width = $measure.find('.thumbnail').width();
+				$measure.remove();
+
+				$thumbnail
+						.css('width', $thumbnail.width() + 'px')
+						.css('height', $thumbnail.height() + 'px');
+
+				var newHeight = $msg.find('p').parent().textHeight(self.smallMessage(), span4Width) + $msg.find('h4').height() + 5;
+
+				$thumbnail.find('.icon-resize-small').removeClass('icon-resize-small').addClass('icon-resize-full');
+				$thumbnail.animate({ width: span4Width, height: newHeight }, 700, function () {
+					$msg.addClass('span4').removeClass('span8');
+					self.currentMessage(self.smallMessage());
+					$msg.css('height', $thumbnail.outerHeight() + 'px');
+
+					updateGrid();
+
+					expanded(false);
+				});
+				$msg.removeClass('expanded');
 			}
 		}
 	});
+	this.toggleExpanded = function () {
+		self.isExpanded(!self.isExpanded());
+
+		return false;
+	};
 }
 
 function ChannelView(channelName) {
