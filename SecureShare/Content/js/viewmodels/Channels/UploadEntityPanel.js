1b9dfd1771dd6f8b4c4c782ca79cf5089279d379
stormbreakerbg <georgyangelov@gmail.com>
2012-07-26 11:14:32 +0300

    Detect which type of entity the user wants to add Fixes #1

--- SecureShare/Content/js/viewmodels/Channels/UploadEntityPanel.js
+++ SecureShare/Content/js/viewmodels/Channels/UploadEntityPanel.js
@@ -41,7 +41,34 @@
 		self.selectedFile(file);
 	};
 
-	this.Submit = function () {
+	this.SubmitFile = function () {
+		//TODO: Actual file upload to 'resourceId: "uploadEntity"'
+	};
+
+	this.SubmitMessage = function () {
+		amplify.request({
+			resourceId: "uploadEntity",
+			data: {
+				channelName: channel.Name(),
+				SessionKey: Application.user().SessionKey.Key(),
+				Title: self.Title(),
+				Message: self.Message()
+			},
+			success: function (data) {
+				Application.alerts.push({ type: "success", title: "Wohoo!", text: "You uploaded a message to " + channel.Name() });
+
+				channel.hideUploadEntityPanel();
+				channel.loadEntities();
+			},
+			error: function (data) {
+				Application.alerts.push({ type: "error", title: "Oops!", text: "Something went completely wrong. Please try again in a few moments" });
+			}
+		});
+
+		return false;
+	};
+
+	this.SubmitLink = function () {
 		amplify.request({
 			resourceId: "uploadEntity",
 			data: {
@@ -52,7 +79,7 @@
 				Link: self.Link()
 			},
 			success: function (data) {
-				Application.alerts.push({ type: "success", title: "Wohoo!", text: "You uploaded an item to " + channel.Name() });
+				Application.alerts.push({ type: "success", title: "Wohoo!", text: "You uploaded a link to " + channel.Name() });
 
 				channel.hideUploadEntityPanel();
 				channel.loadEntities();
